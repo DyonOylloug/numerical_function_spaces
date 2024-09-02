bd3c4f10a10be320fb289c92c021b3a0f0f18fd5
Adam Bohonos <abohonos@zut.edu.pl>
2024-09-02 22:17:30 +0100

    docs: add __version__ in __init__.py

--- src/numerical_function_spaces/orlicz_spaces/__init__.py
+++ src/numerical_function_spaces/orlicz_spaces/__init__.py
@@ -1,3 +1,7 @@
+# read version from installed package
+from importlib.metadata import version
+__version__ = version("numerical_function_spaces")
+
 from .norms import *
 from .orlicz_functions import *
 from .plots import *
